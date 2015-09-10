05cd877c75cb1400be6dc5170e5734089fd42c11
Luciano Zu <luciano.zu@gmail.com>
2015-09-10 09:26:48 +0200

    move DLLs on lib folder in order to run applications from lib dir. Setters 32/64 bit system version batch modified accordinlgy

---
+++ Deploy/rootfolder/bin/set32bitWindowsRXTX.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem copy ..\winDLLs\32bit\RXTXcomm.jar ..\lib
+copy ..\winDLLs\32bit\rxtxSerial.dll ..\lib
+copy ..\winDLLs\32bit\LibusbJava.dll ..\lib
+pause
---
+++ Deploy/rootfolder/bin/set64bitWindowsRXTX.bat
@@ -0,0 +1,6 @@
+@echo off
+
+rem copy ..\winDLLs\64bit\RXTXcomm.jar ..\lib
+copy ..\winDLLs\64bit\rxtxSerial.dll ..\lib
+copy ..\winDLLs\64bit\LibusbJava.dll ..\lib
+pause
