63fca6632b9bf48a270336fd625a5de147bc3769
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2015-10-19 17:51:14 +0200

    #forClassName bug fixed

--- ArdulinkCore/test/org/zu/ardulink/util/PrimitiveTest.java
+++ ArdulinkCore/test/org/zu/ardulink/util/PrimitiveTest.java
@@ -1,7 +1,8 @@
 package org.zu.ardulink.util;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static org.zu.ardulink.util.Primitive.parseAs;
 
 import org.hamcrest.core.Is;
 import org.junit.Test;
@@ -10,10 +11,18 @@ public class PrimitiveTest {
 
 	@Test
 	public void testParseAs() {
-		assertThat(parseAs(int.class, "123"),
+		assertThat(Primitive.parseAs(int.class, "123"),
 				Is.<Object> is(Integer.valueOf(123)));
-		assertThat(parseAs(double.class, "123"),
+		assertThat(Primitive.parseAs(double.class, "123"),
 				Is.<Object> is(Double.valueOf(123)));
+	}
+
+	@Test
+	public void testForClassName() {
+		assertThat(Primitive.forClassName("int"), is(Primitive.INT));
+		assertThat(Primitive.forClassName("double"), is(Primitive.DOUBLE));
+		assertThat(Primitive.forClassName(String.class.getName()),
+				is(nullValue()));
 	}
 
 }
