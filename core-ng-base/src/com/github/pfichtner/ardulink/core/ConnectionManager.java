92ade3c150a4f0a2231331d099756f04ffde79c6
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2015-11-27 00:50:11 +0100

    split unified

--- core-ng-base/src/com/github/pfichtner/ardulink/core/ConnectionManager.java
+++ core-ng-base/src/com/github/pfichtner/ardulink/core/ConnectionManager.java
@@ -31,20 +31,26 @@ public abstract class ConnectionManager {
 								.newConnectionConfig();
 
 						for (String param : params) {
-							boolean configured = configureViaMethodAnnotation(
-									connectionConfig, param);
-							if (!configured) {
-								configured = configureViaFieldAnnotation(
-										connectionConfig, param);
-								if (!configured) {
-									configured = configureViaBeanInfoAnnotation(
-											connectionConfig, param);
-									if (!configured) {
-										// TODO this is key AND value!
-										throw new IllegalArgumentException(
-												"Illegal attribute " + param);
-									}
 
+							String[] split = param.split("\\=");
+							if (split.length == 2) {
+								String key = split[0];
+								String value = split[1];
+
+								boolean configured = configureViaMethodAnnotation(
+										connectionConfig, key, value);
+								if (!configured) {
+									configured = configureViaFieldAnnotation(
+											connectionConfig, key, value);
+									if (!configured) {
+										configured = configureViaBeanInfoAnnotation(
+												connectionConfig, key, value);
+										if (!configured) {
+											throw new IllegalArgumentException(
+													"Illegal attribute " + key);
+										}
+
+									}
 								}
 							}
 						}
@@ -57,80 +63,63 @@ public abstract class ConnectionManager {
 			}
 
 			private boolean configureViaMethodAnnotation(
-					ConnectionConfig connectionConfig, String param) {
-				String[] split = param.split("\\=");
-				if (split.length == 2) {
-					String key = split[0];
-					String value = split[1];
-					try {
+					ConnectionConfig connectionConfig, String key, String value) {
+				try {
 
-						for (Method method : connectionConfig.getClass()
-								.getMethods()) {
-							if (method != null
-									&& method.isAnnotationPresent(Name.class)
-									&& method.getParameterTypes().length == 1
-									&& key.equals(method.getAnnotation(
-											Name.class).value())) {
-								setViaMethod(connectionConfig, method, key,
-										value);
-								return true;
-							}
+					for (Method method : connectionConfig.getClass()
+							.getMethods()) {
+						if (method != null
+								&& method.isAnnotationPresent(Name.class)
+								&& method.getParameterTypes().length == 1
+								&& key.equals(method.getAnnotation(Name.class)
+										.value())) {
+							setViaMethod(connectionConfig, method, key, value);
+							return true;
 						}
-					} catch (IllegalArgumentException e) {
-						throw new RuntimeException(e);
 					}
+				} catch (IllegalArgumentException e) {
+					throw new RuntimeException(e);
 				}
 				return false;
 			}
 
 			private boolean configureViaFieldAnnotation(
-					ConnectionConfig connectionConfig, String param) {
-				String[] split = param.split("\\=");
-				if (split.length == 2) {
-					String key = split[0];
-					String value = split[1];
-					try {
+					ConnectionConfig connectionConfig, String key, String value) {
+				try {
 
-						for (Field field : connectionConfig.getClass()
-								.getFields()) {
-							if (field != null
-									&& field.isAnnotationPresent(Name.class)
-									&& key.equals(field.getAnnotation(
-											Name.class).value())) {
-								setViaField(connectionConfig, field, key, value);
-								return true;
-							}
+					for (Field field : connectionConfig.getClass().getFields()) {
+						if (field != null
+								&& field.isAnnotationPresent(Name.class)
+								&& key.equals(field.getAnnotation(Name.class)
+										.value())) {
+							setViaField(connectionConfig, field, key, value);
+							return true;
 						}
-					} catch (IllegalArgumentException e) {
-						throw new RuntimeException(e);
 					}
+				} catch (IllegalArgumentException e) {
+					throw new RuntimeException(e);
 				}
 				return false;
 			}
 
 			private boolean configureViaBeanInfoAnnotation(
-					ConnectionConfig connectionConfig, String param) {
-				String[] split = param.split("\\=");
-				if (split.length == 2) {
-					String key = split[0];
-					String value = split[1];
-					try {
-						BeanInfo beanInfo = Introspector
-								.getBeanInfo(connectionConfig.getClass());
-						PropertyDescriptor[] propertyDescriptors = beanInfo
-								.getPropertyDescriptors();
-						for (PropertyDescriptor pd : propertyDescriptors) {
-							if (pd.getName().equals(key)
-									&& pd.getWriteMethod() != null) {
-								setValue(connectionConfig, pd, key, value);
-								return true;
-							}
+					ConnectionConfig connectionConfig, String key, String value) {
+				try {
+					BeanInfo beanInfo = Introspector
+							.getBeanInfo(connectionConfig.getClass());
+					PropertyDescriptor[] propertyDescriptors = beanInfo
+							.getPropertyDescriptors();
+					for (PropertyDescriptor pd : propertyDescriptors) {
+						if (pd.getName().equals(key)
+								&& pd.getWriteMethod() != null) {
+							setValue(connectionConfig, pd, key, value);
+							return true;
 						}
-					} catch (IntrospectionException e) {
-						throw new RuntimeException(e);
-					} catch (IllegalArgumentException e) {
-						throw new RuntimeException(e);
 					}
+				} catch (IntrospectionException e) {
+					throw new RuntimeException(e);
+				} catch (IllegalArgumentException e) {
+					throw new RuntimeException(e);
 				}
 				return false;
 			}
