223d80533149cb8077bfb3b3fcf051d87f275887
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2015-11-28 21:26:21 +0100

    house keeping

--- core-ng-beans/src/com/github/pfichtner/beans/finder/impl/FindByAnnotation.java
+++ core-ng-beans/src/com/github/pfichtner/beans/finder/impl/FindByAnnotation.java
@@ -9,7 +9,6 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
