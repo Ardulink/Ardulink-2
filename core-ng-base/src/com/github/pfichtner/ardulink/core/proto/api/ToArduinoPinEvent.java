b114349a4b1170ef355792b07b849b1635cd5a7d
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2015-12-05 22:58:51 +0100

    no object constructor

--- core-ng-base/src/com/github/pfichtner/ardulink/core/proto/api/ToArduinoPinEvent.java
+++ core-ng-base/src/com/github/pfichtner/ardulink/core/proto/api/ToArduinoPinEvent.java
@@ -1,13 +1,20 @@
 package com.github.pfichtner.ardulink.core.proto.api;
 
 import com.github.pfichtner.ardulink.core.Pin;
+import com.github.pfichtner.ardulink.core.Pin.AnalogPin;
+import com.github.pfichtner.ardulink.core.Pin.DigitalPin;
 
 public class ToArduinoPinEvent {
 
 	public final Pin pin;
 	public final Object value;
 
-	public ToArduinoPinEvent(Pin pin, Object value) {
+	public ToArduinoPinEvent(DigitalPin pin, Boolean value) {
+		this.pin = pin;
+		this.value = value;
+	}
+
+	public ToArduinoPinEvent(AnalogPin pin, Integer value) {
 		this.pin = pin;
 		this.value = value;
 	}
