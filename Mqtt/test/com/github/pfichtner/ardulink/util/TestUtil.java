53ae7f37ef4166a23abb5c249e8fdb1a8e443a3c
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2015-12-08 23:29:31 +0100

    purged unused methods

--- Mqtt/test/com/github/pfichtner/ardulink/util/TestUtil.java
+++ Mqtt/test/com/github/pfichtner/ardulink/util/TestUtil.java
@@ -18,20 +18,14 @@ package com.github.pfichtner.ardulink.util;
 
 import static com.github.pfichtner.ardulink.core.Pin.analogPin;
 import static com.github.pfichtner.ardulink.core.Pin.digitalPin;
-import static java.util.Collections.singletonList;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.dna.mqtt.moquette.server.Server;
-import org.zu.ardulink.ConnectionContact;
-import org.zu.ardulink.connection.Connection;
-import org.zu.ardulink.connection.serial.AbstractSerialConnection;
 
 import com.github.pfichtner.ardulink.MqttMain;
 import com.github.pfichtner.ardulink.core.events.AnalogPinValueChangedEvent;
@@ -50,62 +44,6 @@ public final class TestUtil {
 
 	private TestUtil() {
 		super();
-	}
-
-	public static Connection createConnection(final OutputStream outputStream,
-			ConnectionContact connectionContact) {
-		return new AbstractSerialConnection(connectionContact) {
-
-			{
-				setOutputStream(outputStream);
-			}
-
-			@Override
-			public List<String> getPortList() {
-				return singletonList("/dev/null");
-			}
-
-			@Override
-			public boolean disconnect() {
-				setConnected(false);
-				return isConnected();
-			}
-
-			@Override
-			public boolean connect(Object... params) {
-				setConnected(true);
-				return isConnected();
-			}
-		};
-	}
-
-	public static int[] toCodepoints(String message) {
-		int[] codepoints = new int[message.length()];
-		for (int i = 0; i < message.length(); i++) {
-			codepoints[i] = message.codePointAt(i);
-		}
-		return codepoints;
-	}
-
-	public static Field getField(Object target, String fieldName) {
-		try {
-			return target.getClass().getDeclaredField(fieldName);
-		} catch (NoSuchFieldException e) {
-			throw new IllegalStateException(e);
-		} catch (SecurityException e) {
-			throw new IllegalStateException(e);
-		}
-	}
-
-	public static void set(Object target, Field field, Object instance) {
-		field.setAccessible(true);
-		try {
-			field.set(target, instance);
-		} catch (IllegalArgumentException e) {
-			throw new IllegalStateException(e);
-		} catch (IllegalAccessException e) {
-			throw new IllegalStateException(e);
-		}
 	}
 
 	public static Server startBroker() throws IOException, InterruptedException {
