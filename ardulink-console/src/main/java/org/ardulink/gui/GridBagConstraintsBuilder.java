5aeeeb4894514149275416e497f3bd51e03c44ce
Peter Fichtner (pfichtner) <github.2012.07.fichtner@spamgourmet.com>
2016-04-12 19:51:36 +0200

    house keeping

--- ardulink-console/src/main/java/org/ardulink/gui/GridBagConstraintsBuilder.java
+++ ardulink-console/src/main/java/org/ardulink/gui/GridBagConstraintsBuilder.java
@@ -18,41 +18,79 @@ package org.ardulink.gui;
 import static java.awt.GridBagConstraints.BOTH;
 import static java.awt.GridBagConstraints.HORIZONTAL;
 import static java.awt.GridBagConstraints.LINE_START;
+import static java.awt.GridBagConstraints.VERTICAL;
 
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
 public final class GridBagConstraintsBuilder {
 
-	private final GridBagConstraints c;
+	private final GridBagConstraints c = new GridBagConstraints();
 
-	private GridBagConstraintsBuilder(int row, int column) {
-		c = new GridBagConstraints();
-		c.anchor = LINE_START;
-		c.gridx = column;
-		c.gridy = row;
-		c.insets = new Insets(4, 4, 4, 4);
+	private GridBagConstraintsBuilder() {
+		super();
 	}
 
 	public static GridBagConstraintsBuilder constraints(int row, int column) {
-		return new GridBagConstraintsBuilder(row, column);
+		return new GridBagConstraintsBuilder().row(row).column(column)
+				.anchor(LINE_START).insets(4, 4, 4, 4);
 	}
 
-	public GridBagConstraintsBuilder fillHorizontal() {
-		c.weightx = 1;
-		c.fill = HORIZONTAL;
+	public GridBagConstraintsBuilder row(int row) {
+		c.gridy = row;
+		return this;
+	}
+
+	public GridBagConstraintsBuilder column(int column) {
+		c.gridx = column;
+		return this;
+	}
+
+	public GridBagConstraintsBuilder insets(int top, int left, int bottom,
+			int right) {
+		c.insets = new Insets(top, left, bottom, right);
+		return this;
+	}
+
+	public GridBagConstraintsBuilder anchor(int anchor) {
+		c.anchor = anchor;
 		return this;
 	}
 
 	public GridBagConstraintsBuilder fillBoth() {
-		c.weightx = 1;
-		c.weighty = 1;
-		c.fill = BOTH;
+		return fillHorizontal().fillVertical().fill(BOTH);
+	}
+
+	public GridBagConstraintsBuilder fillHorizontal() {
+		return weightX(1).fill(HORIZONTAL);
+	}
+
+	public GridBagConstraintsBuilder fillVertical() {
+		return weightY(1).fill(VERTICAL);
+	}
+
+	public GridBagConstraintsBuilder weightX(int weight) {
+		c.weightx = weight;
+		return this;
+	}
+
+	public GridBagConstraintsBuilder weightY(int weight) {
+		c.weighty = weight;
+		return this;
+	}
+
+	private GridBagConstraintsBuilder fill(int type) {
+		c.fill = type;
 		return this;
 	}
 
 	public GridBagConstraintsBuilder gridwidth(int width) {
 		c.gridwidth = width;
+		return this;
+	}
+
+	public GridBagConstraintsBuilder gridheight(int height) {
+		c.gridheight = height;
 		return this;
 	}
 
