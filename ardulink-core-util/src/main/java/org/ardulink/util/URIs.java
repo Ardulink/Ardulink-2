8e8cdf7b01fd5a5871ac2b2bbc0f8c9d2371c120
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2017-03-30 22:34:45 +0200

    cleanup

--- ardulink-core-util/src/main/java/org/ardulink/util/URIs.java
+++ ardulink-core-util/src/main/java/org/ardulink/util/URIs.java
@@ -1,7 +1,11 @@
 package org.ardulink.util;
 
+import static org.ardulink.util.Preconditions.checkNotNull;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 
 public final class URIs {
 
@@ -11,8 +15,19 @@ public final class URIs {
 
 	public static URI newURI(String asciiString) {
 		try {
-			return new URI((asciiString==null)?null:asciiString.replaceAll(" ", "%20"));
+			return new URI(checkNotNull(asciiString,
+					"asciiString must not be null"));
 		} catch (URISyntaxException e) {
+			throw Throwables.propagate(e);
+		}
+	}
+
+	public static String encode(String asciiString) {
+		try {
+			return URLEncoder.encode(
+					checkNotNull(asciiString, "asciiString must not be null"),
+					"UTF-8");
+		} catch (UnsupportedEncodingException e) {
 			throw Throwables.propagate(e);
 		}
 	}
--- ardulink-core-util/src/test/java/org/ardulink/util/URIsTest.java
+++ ardulink-core-util/src/test/java/org/ardulink/util/URIsTest.java
@@ -1,25 +1,55 @@
 package org.ardulink.util;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class URIsTest {
-	
-	@Test
-	public void simpleURI() {
-		
-		URI uri = URIs.newURI("ardulink://serial-jssc?port=COM3");
-		assertEquals(uri.getQuery(), "port=COM3");
+
+	// TODO if needed make URLBuilder public and convert to top-level-class. If
+	// not needed, remove whole test class including URLBuilder
+	public static class URLBuilder {
+
+		private final StringBuilder base;
+
+		public URLBuilder(String base) {
+			this.base = new StringBuilder(base);
+			if (!base.endsWith("?")) {
+				this.base.append('?');
+			}
+		}
+
+		public URLBuilder param(String name, String value) {
+			this.base.append(name).append('=').append(URIs.encode(value))
+					.append('&');
+			return this;
+		}
+
+		public URI build() {
+			String string = this.base.toString();
+			return URIs.newURI(string.endsWith("&") ? string.substring(0,
+					string.length() - 1) : string);
+		}
+
 	}
 
 	@Test
-	public void queryURIWithSpaceChar() {
-		
-		URI uri = URIs.newURI("http://serial-jssc?port=COM3&other=good enough");
-		System.out.println(uri.getQuery());
-		assertEquals(uri.getQuery(), "port=COM3&other=good enough");
+	public void simpleURI() throws URISyntaxException {
+		URI uri = new URLBuilder("ardulink://serial-jssc")
+				.param("port", "COM3").build();
+		assertThat(uri, is(new URI("ardulink://serial-jssc?port=COM3")));
 	}
+
+	@Test
+	public void queryURIWithSpaceChar() throws URISyntaxException {
+		URI uri = new URLBuilder("http://serial-jssc").param("port", "COM3")
+				.param("other", "good enough").build();
+		assertThat(uri, is(new URI(
+				"http://serial-jssc?port=COM3&other=good+enough")));
+	}
+
 }
