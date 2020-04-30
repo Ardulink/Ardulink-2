e72b2b37412cd56296512d79fa813081a84e9a61
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-04-30 08:55:15 +0200

    switching pins via HTTP post

--- ardulink-rest/src/test/java/ardulink/rest/ArdulinkRestTest.java
+++ ardulink-rest/src/test/java/ardulink/rest/ArdulinkRestTest.java
@@ -1,7 +1,7 @@
 package ardulink.rest;
 
 import static io.restassured.RestAssured.port;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 import static java.lang.Boolean.parseBoolean;
 import static java.util.Arrays.asList;
 import static org.ardulink.core.proto.impl.ALProtoBuilder.alpProtocolMessage;
@@ -31,7 +31,7 @@ public class ArdulinkRestTest {
 		try (CamelContext context = startCamelRest(out)) {
 			int pin = 5;
 			boolean state = true;
-			when().get("/pin/digital/{pin}/{state}", pin, state).then().statusCode(200);
+			given().body(state).post("/pin/digital/{pin}", pin).then().statusCode(200);
 			MockEndpoint endpoint = mockEndpoint(context, out);
 			endpoint.expectedBodiesReceived(asList(alpProtocolMessage(DIGITAL_PIN_READ).forPin(pin).withState(state)));
 			endpoint.assertIsSatisfied();
@@ -45,7 +45,7 @@ public class ArdulinkRestTest {
 		try (CamelContext context = startCamelRest(out)) {
 			int pin = 9;
 			int value = 123;
-			when().get("/pin/analog/{pin}/{value}", pin, value).then().statusCode(200);
+			given().body(value).post("/pin/analog/{pin}", pin).then().statusCode(200);
 			MockEndpoint endpoint = mockEndpoint(context, out);
 			endpoint.expectedBodiesReceived(asList(alpProtocolMessage(ANALOG_PIN_READ).forPin(pin).withValue(value)));
 			endpoint.assertIsSatisfied();
@@ -65,25 +65,25 @@ public class ArdulinkRestTest {
 				restConfiguration().host("localhost").port(port);
 				rest("/pin") //
 						.consumes("application/json").produces("application/json") //
-						.get("/analog/{pin}/{value}").to("direct:switchAnalog") //
-						.get("/digital/{pin}/{state}").to("direct:switchDigital") //
+						.post("/analog/{pin}").to("direct:switchAnalog") //
+						.post("/digital/{pin}").to("direct:switchDigital") //
 				;
 
 				from("direct:switchAnalog").process(exchange -> {
 					Message message = exchange.getMessage();
 					Object pinRaw = message.getHeader("pin");
-					Object valueRaw = message.getHeader("value");
+					String valueRaw = message.getBody(String.class);
 					int pin = tryParse(String.valueOf(pinRaw)).getOrThrow("Pin %s not parseable", pinRaw);
-					int value = tryParse(String.valueOf(valueRaw)).getOrThrow("Value %s not parseable", valueRaw);
+					int value = tryParse(valueRaw).getOrThrow("Value %s not parseable", valueRaw);
 					message.setBody(alpProtocolMessage(ANALOG_PIN_READ).forPin(pin).withValue(value));
 				}).to(out);
 
 				from("direct:switchDigital").process(exchange -> {
 					Message message = exchange.getMessage();
 					Object pinRaw = message.getHeader("pin");
-					Object stateRaw = message.getHeader("state");
+					String stateRaw = message.getBody(String.class);
 					int pin = tryParse(String.valueOf(pinRaw)).getOrThrow("Pin %s not parseable", pinRaw);
-					boolean state = parseBoolean(String.valueOf(stateRaw));
+					boolean state = parseBoolean(stateRaw);
 					message.setBody(alpProtocolMessage(DIGITAL_PIN_READ).forPin(pin).withState(state));
 				}).to(out);
 			}
