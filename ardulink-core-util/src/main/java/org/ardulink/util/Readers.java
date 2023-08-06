874a308363af1ee8f1f523d005f8a0a0a8afce8b
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2023-08-06 17:26:31 +0200

    separated Streams/Readers

--- ardulink-core-util/src/main/java/org/ardulink/util/Readers.java
+++ ardulink-core-util/src/main/java/org/ardulink/util/Readers.java
@@ -16,26 +16,30 @@ limitations under the License.
 package org.ardulink.util;
 
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.stream.Stream;
 
-public final class InputStreams {
+public final class Readers {
 
 	private static final String lineSeparator = System.getProperty("line.separator");
 
-	private InputStreams() {
+	private Readers() {
 		super();
 	}
 
-	public static String toString(InputStream inputStream) throws IOException {
-		return lines(inputStream).collect(joining(lineSeparator));
+	public static String toString(Reader reader) throws IOException {
+		return lines(reader).collect(joining(lineSeparator));
 	}
 
-	public static Stream<String> lines(InputStream inputStream) throws IOException {
-		return Readers.lines(new InputStreamReader(inputStream));
+	public static Stream<String> lines(Reader reader) throws IOException {
+		try (BufferedReader bufferedReader = new BufferedReader(reader)) {
+			// have to consume the stream because otherwise it's closed outside try/catch
+			return bufferedReader.lines().collect(toList()).stream();
+		}
 	}
 
 }
---
+++ ardulink-core-util/src/test/java/org/ardulink/util/InputStreamsTest.java
@@ -0,0 +1,57 @@
+/**
+Copyright 2013 project Ardulink http://www.ardulink.org/
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package org.ardulink.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * [ardulinktitle] [ardulinkversion]
+ * 
+ * project Ardulink http://www.ardulink.org/
+ * 
+ * [adsense]
+ *
+ */
+class InputStreamsTest {
+
+	String lineSeparator = System.getProperty("line.separator");
+
+	ByteArrayInputStream empty() {
+		return new ByteArrayInputStream("".getBytes());
+	}
+
+	ByteArrayInputStream fooBar() {
+		return new ByteArrayInputStream("foo\nbar".getBytes());
+	}
+
+	@Test
+	void testLines() throws IOException {
+		assertThat(InputStreams.lines(empty())).isEmpty();
+		assertThat(InputStreams.lines(fooBar())).containsExactly("foo", "bar");
+	}
+
+	@Test
+	void testToString() throws IOException {
+		assertThat(InputStreams.toString(empty())).isEmpty();
+		assertThat(InputStreams.toString(fooBar())).isEqualTo("foo" + lineSeparator + "bar");
+	}
+
+}
