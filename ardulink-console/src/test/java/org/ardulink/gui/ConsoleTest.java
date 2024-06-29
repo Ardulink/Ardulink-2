74d7026bc019d6b8b86f9c3bab5af3994e74544a
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-06-29 23:49:16 +0200

    search logic moved to seperate class

--- ardulink-console/src/test/java/org/ardulink/gui/ConsoleTest.java
+++ ardulink-console/src/test/java/org/ardulink/gui/ConsoleTest.java
@@ -1,34 +1,49 @@
+/**
+Copyright 2013 project Ardulink http://www.ardulink.org/
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package org.ardulink.gui;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.lang.String.format;
-import static java.util.stream.IntStream.range;
-import static javax.swing.SwingUtilities.invokeAndWait;
-import static javax.swing.SwingUtilities.invokeLater;
 import static org.ardulink.core.linkmanager.LinkManager.ARDULINK_SCHEME;
 import static org.ardulink.core.virtual.console.VirtualConnectionLinkFactory.VIRTUAL_CONSOLE_NAME;
+import static org.ardulink.gui.SwingSelector.containsItem;
+import static org.ardulink.gui.SwingSelector.findComponent;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.stream.Stream;
-
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
-import javax.swing.RepaintManager;
-import javax.swing.SwingUtilities;
 
 import org.approvaltests.awt.AwtApprovals;
 import org.ardulink.legacy.Link;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIf;
 
+/**
+ * [ardulinktitle] [ardulinkversion] This is the ready ardulink console a
+ * complete SWING application to manage an Arduino board. Console has several
+ * tabs with all ready arduino components. Each tab is able to do a specific
+ * action sending or listening for messages to arduino or from arduino board.
+ * 
+ * project Ardulink http://www.ardulink.org/
+ * 
+ * [adsense]
+ *
+ */
 class ConsoleTest {
 
 	private static final String IS_HEADLESS = "java.awt.GraphicsEnvironment#isHeadless";
@@ -72,46 +87,14 @@ class ConsoleTest {
 	void approvalsVerify() throws Exception {
 		Console console = newConsole();
 		String name = format("%s://%s", ARDULINK_SCHEME, VIRTUAL_CONSOLE_NAME);
-
-		JComboBox<?> comboBox = findComboBoxContainingItem(console, name);
-		comboBox.setSelectedItem(name);
+		findComponent(console, JComboBox.class, c -> containsItem(c, name)).setSelectedItem(name);
 		repaint(console);
 
 		AwtApprovals.verify(console.getContentPane());
 	}
 
 	private void repaint(Console console) {
-		console.repaint();
-		await().until(console::isOpaque);
-	}
-
-	private static JComboBox<?> findComboBoxContainingItem(Container container, Object item) {
-		return findComponentRecursively(container, JComboBox.class) //
-				.filter(comboBox -> containsItem((JComboBox<?>) comboBox, item)) //
-				.map(comboBox -> (JComboBox<?>) comboBox) //
-				.findFirst() //
-				.orElse(null) //
-		;
-	}
-
-	private static <T extends Component> Stream<T> findComponentRecursively(Container container, Class<T> clazz) {
-		return Arrays.stream(container.getComponents()).flatMap(comp -> {
-			if (clazz.isInstance(comp)) {
-				return Stream.of(clazz.cast(comp));
-			} else if (comp instanceof Container) {
-				return findComponentRecursively((Container) comp, clazz);
-			} else {
-				return Stream.empty();
-			}
-		});
-	}
-
-	private static boolean containsItem(JComboBox<?> comboBox, Object item) {
-		return Arrays.stream(range(0, comboBox.getItemCount()) //
-				.mapToObj(comboBox::getItemAt) //
-				.toArray()) //
-				.anyMatch(e -> Objects.equals(e, item)) //
-		;
+		await().until(() -> console.isOpaque());
 	}
 
 	@SuppressWarnings("serial")
---
+++ ardulink-console/src/test/java/org/ardulink/gui/SwingSelector.java
@@ -0,0 +1,75 @@
+/**
+Copyright 2013 project Ardulink http://www.ardulink.org/
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package org.ardulink.gui;
+
+import static java.util.stream.IntStream.range;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import javax.swing.JComboBox;
+
+/**
+ * [ardulinktitle] [ardulinkversion] This is the ready ardulink console a
+ * complete SWING application to manage an Arduino board. Console has several
+ * tabs with all ready arduino components. Each tab is able to do a specific
+ * action sending or listening for messages to arduino or from arduino board.
+ * 
+ * project Ardulink http://www.ardulink.org/
+ * 
+ * [adsense]
+ *
+ */
+public final class SwingSelector {
+
+	private SwingSelector() {
+		super();
+	}
+
+	public static <T extends Component> T findComponent(Container container, Class<T> type,
+			Predicate<? super T> predicate) {
+		return findComponentRecursively(container, type) //
+				.filter(predicate) //
+				.map(type::cast) //
+				.findFirst() //
+				.orElse(null) //
+		;
+	}
+
+	private static <T extends Component> Stream<T> findComponentRecursively(Container container, Class<T> clazz) {
+		return Arrays.stream(container.getComponents()).flatMap(c -> {
+			if (clazz.isInstance(c)) {
+				return Stream.of(clazz.cast(c));
+			} else if (c instanceof Container) {
+				return findComponentRecursively((Container) c, clazz);
+			}
+			return Stream.empty();
+		});
+	}
+
+	public static boolean containsItem(JComboBox<?> comboBox, Object item) {
+		return Arrays.stream(range(0, comboBox.getItemCount()) //
+				.mapToObj(comboBox::getItemAt) //
+				.toArray()) //
+				.anyMatch(e -> Objects.equals(e, item)) //
+		;
+	}
+
+}
