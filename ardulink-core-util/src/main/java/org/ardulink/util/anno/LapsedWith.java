7c82447228d4b2340cc140837e08ef77f6d7cfa0
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-11-17 10:20:50 +0100

    removed foreach/Stream-example since we already migrated to JDK8

--- ardulink-core-util/src/main/java/org/ardulink/util/anno/LapsedWith.java
+++ ardulink-core-util/src/main/java/org/ardulink/util/anno/LapsedWith.java
@@ -6,10 +6,8 @@ import java.lang.annotation.Retention;
 
 /**
  * Elements marked with this annotation can be replaced by more efficient or
- * easier to read alternatives. For example some for-loops can be replaced by
- * Stream when the enclosing Ardulink module or one of it dependencies is
- * updated. To make search as easy as possible when upgrading, value
- * <b>should</b> be one of the constant values.
+ * easier to read alternatives. To make search as easy as possible when
+ * upgrading, value <b>should</b> be one of the constant values.
  */
 @Retention(SOURCE)
 public @interface LapsedWith {
