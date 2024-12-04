cb457d36860f729a0fcbdbb17179c29c05cd2f36
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-12-04 13:20:07 +0100

    ServiceLoader Java9 migration

--- ardulink-core-util/src/main/java/org/ardulink/util/ServiceLoaders.java
+++ ardulink-core-util/src/main/java/org/ardulink/util/ServiceLoaders.java
@@ -16,12 +16,9 @@ limitations under the License.
 
 package org.ardulink.util;
 
-import static org.ardulink.util.anno.LapsedWith.JDK9;
-
 import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider;
 import java.util.stream.Stream;
-
-import org.ardulink.util.anno.LapsedWith;
 
 /**
  * [ardulinktitle] [ardulinkversion]
@@ -37,19 +34,16 @@ public final class ServiceLoaders {
 		super();
 	}
 
-	@LapsedWith(module = JDK9, value = "ServiceLoader#stream")
 	public static <T> Stream<T> services(Class<T> type) {
 		return toStream(ServiceLoader.load(type));
 	}
 
-	@LapsedWith(module = JDK9, value = "ServiceLoader#stream")
 	public static <T> Stream<T> services(Class<T> type, ClassLoader classloader) {
 		return toStream(ServiceLoader.load(type, classloader));
 	}
 
-	@LapsedWith(module = JDK9, value = "ServiceLoader#stream")
 	private static <T> Stream<T> toStream(ServiceLoader<T> serviceLoader) {
-		return Iterators.stream(serviceLoader.iterator());
+		return serviceLoader.stream().map(Provider::get);
 	}
 
 }
