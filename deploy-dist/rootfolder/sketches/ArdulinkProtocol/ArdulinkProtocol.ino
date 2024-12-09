966edd6df436adb689f3b481ba2fe873f93f6086
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-12-09 13:45:48 +0100

    do not append delimiter

--- deploy-dist/rootfolder/sketches/ArdulinkProtocol/ArdulinkProtocol.ino
+++ deploy-dist/rootfolder/sketches/ArdulinkProtocol/ArdulinkProtocol.ino
@@ -219,12 +219,13 @@ void serialEvent() {
   while (Serial.available() && !stringComplete) {
     // get the new byte:
     char inChar = (char)Serial.read();
-    // add it to the inputString:
-    inputString += inChar;
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
     if (inChar == '\n') {
       stringComplete = true;
+    } else {
+      // add it to the inputString:
+      inputString += inChar;
     }
   }
 }
