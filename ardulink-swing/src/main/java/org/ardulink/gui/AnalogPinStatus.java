5774de28aba4681855f4a4806ec1f3a35caf2969
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-02-22 19:26:12 +0100

    house keeping

--- ardulink-swing/src/main/java/org/ardulink/gui/AnalogPinStatus.java
+++ ardulink-swing/src/main/java/org/ardulink/gui/AnalogPinStatus.java
@@ -15,6 +15,8 @@ limitations under the License.
 */
 package org.ardulink.gui;
 
+import static java.awt.event.ItemEvent.DESELECTED;
+import static java.awt.event.ItemEvent.SELECTED;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 import static org.ardulink.core.Pin.analogPin;
@@ -42,16 +44,15 @@ import org.ardulink.gui.facility.IntMinMaxModel;
 import org.ardulink.util.Throwables;
 
 /**
- * [ardulinktitle] [ardulinkversion]
- * This class implements the AnalogReadChangeListener interface and is able to listen
- * events coming from arduino board about analog pin state change.
- * project Ardulink http://www.ardulink.org/
+ * [ardulinktitle] [ardulinkversion] This class implements the
+ * AnalogReadChangeListener interface and is able to listen events coming from
+ * arduino board about analog pin state change. project Ardulink
+ * http://www.ardulink.org/
  * 
  * [adsense]
  *
  */
 public class AnalogPinStatus extends JPanel implements Linkable {
-
 
 	private static final Font FONT_11 = new Font("SansSerif", Font.PLAIN, 11);
 	private static final Font FONT_12 = new Font("SansSerif", Font.PLAIN, 12);
@@ -69,7 +70,7 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 	private IntMinMaxModel pinComboBoxModel;
 	private JLabel lblPowerPinController;
 	private JToggleButton tglbtnSensor;
-	
+
 	private transient EventListener listener;
 
 	private FilteredEventListenerAdapter listener() {
@@ -98,20 +99,22 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 	public AnalogPinStatus() {
 		setPreferredSize(new Dimension(140, 260));
 		setLayout(null);
-		
+
 		JLabel lblPowerPin = new JLabel("Power Pin:");
 		lblPowerPin.setFont(FONT_11);
 		lblPowerPin.setBounds(10, 40, 59, 14);
 		add(lblPowerPin);
-	
-		// TODO define a method to be able to change the set of controllable pins. This way you can work with different boards than an Arduino UNO
-		// pinComboBox.setModel(new DefaultComboBoxModel(new String[] {"3", "5", "6", "9", "10", "11"}));
+
+		// TODO define a method to be able to change the set of controllable pins. This
+		// way you can work with different boards than an Arduino UNO
+		// pinComboBox.setModel(new DefaultComboBoxModel(new String[] {"3", "5", "6",
+		// "9", "10", "11"}));
 		pinComboBoxModel = new IntMinMaxModel(0, 40);
 		pinComboBox = new JComboBox<>(pinComboBoxModel);
 		pinComboBox.setSelectedItem(Integer.valueOf(0));
 		pinComboBox.setBounds(65, 36, 62, 22);
 		add(pinComboBox);
-		
+
 		maxValueComboBoxModel = new IntMinMaxModel(0, 1023).withLastItemSelected();
 		maxValueComboBox = new JComboBox<>(maxValueComboBoxModel);
 		maxValueComboBox.setBounds(65, 65, 62, 22);
@@ -121,17 +124,17 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 		minValueComboBox = new JComboBox<>(minValueComboBoxModel);
 		minValueComboBox.setBounds(65, 217, 62, 22);
 		add(minValueComboBox);
-		
+
 		JLabel lblMaxValue = new JLabel("Max Value:");
 		lblMaxValue.setFont(FONT_11);
 		lblMaxValue.setBounds(10, 69, 59, 14);
 		add(lblMaxValue);
-		
+
 		JLabel lblMinValue = new JLabel("Min Value:");
 		lblMinValue.setFont(FONT_11);
 		lblMinValue.setBounds(10, 221, 59, 14);
 		add(lblMinValue);
-		
+
 		progressBar = new JProgressBar();
 		progressBar.setFont(FONT_11);
 		progressBar.setStringPainted(true);
@@ -139,54 +142,52 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 		progressBar.setOrientation(SwingConstants.VERTICAL);
 		progressBar.setBounds(111, 99, 16, 108);
 		add(progressBar);
-		
+
 		lblPowerPinController = new JLabel("Analog PIN Status");
 		lblPowerPinController.setFont(FONT_12);
 		lblPowerPinController.setToolTipText("");
 		lblPowerPinController.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPowerPinController.setBounds(10, 11, 117, 14);
 		add(lblPowerPinController);
-		
+
 		JLabel lblVoltOutput = new JLabel("Volt Output:");
 		lblVoltOutput.setFont(FONT_11);
 		lblVoltOutput.setBounds(10, 143, 59, 14);
 		add(lblVoltOutput);
-		
+
 		voltValueLbl = new JLabel("0V");
 		voltValueLbl.setFont(FONT_11);
 		voltValueLbl.setBounds(10, 157, 76, 14);
 		add(voltValueLbl);
-		
+
 		JLabel lblCurrentValue = new JLabel("Current Value:");
 		lblCurrentValue.setFont(FONT_11);
 		lblCurrentValue.setBounds(10, 98, 76, 14);
 		add(lblCurrentValue);
-		
+
 		valueLabel = new JLabel();
 		valueLabel.setBounds(10, 112, 55, 22);
 		valueLabel.setText("0");
 		add(valueLabel);
-		
+
 		tglbtnSensor = new JToggleButton("Off");
 		tglbtnSensor.addItemListener(new ItemListener() {
 
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				try {
-					if (e.getStateChange() == ItemEvent.SELECTED) {
+					if (e.getStateChange() == SELECTED) {
 						link.addListener((listener = listener()));
-						
+
 						tglbtnSensor.setText("On");
 						pinComboBox.setEnabled(false);
 						minValueComboBox.setEnabled(false);
 						maxValueComboBox.setEnabled(false);
-						
+
 						progressBar.setEnabled(true);
-						
-						
-					} else if(e.getStateChange() == ItemEvent.DESELECTED) {
+					} else if (e.getStateChange() == DESELECTED) {
 						link.removeListener(listener);
-						
+
 						tglbtnSensor.setText("Off");
 						pinComboBox.setEnabled(true);
 						minValueComboBox.setEnabled(true);
@@ -201,18 +202,18 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 		});
 		tglbtnSensor.setBounds(10, 177, 76, 28);
 		add(tglbtnSensor);
-		
-		minValueComboBox.addActionListener(e -> {
+
+		minValueComboBox.addActionListener(__ -> {
 			int maximum = getMaxValue();
 			int minimum = getMinValue();
-			
+
 			if (minimum > maximum) {
 				minValueComboBoxModel.setSelectedItem(maximum);
 			}
 			updateValue();
 		});
 
-		maxValueComboBox.addActionListener(e -> {
+		maxValueComboBox.addActionListener(__ -> {
 			int maximum = getMaxValue();
 			int minimum = getMinValue();
 
@@ -223,9 +224,10 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 		});
 
 	}
-	
+
 	/**
 	 * Set the pin to control
+	 * 
 	 * @param pin the pin number to control
 	 */
 	public void setPin(int pin) {
@@ -247,24 +249,23 @@ public class AnalogPinStatus extends JPanel implements Linkable {
 	public void setTitle(String title) {
 		lblPowerPinController.setText(title);
 	}
-		
+
 	public int getValue() {
 		return Integer.parseInt(valueLabel.getText());
 	}
 
 	public void setValue(int value) {
-		valueLabel.setText(String.valueOf(max(getMinValue(),
-				min(value, getMaxValue()))));
+		valueLabel.setText(String.valueOf(max(getMinValue(), min(value, getMaxValue()))));
 	}
 
 	public int getMinValue() {
 		return minValueComboBoxModel.getSelectedItem().intValue();
 	}
-	
+
 	public int getMaxValue() {
 		return maxValueComboBoxModel.getSelectedItem().intValue();
 	}
-	
+
 	private void updateValue() {
 		setValue(getValue());
 	}
