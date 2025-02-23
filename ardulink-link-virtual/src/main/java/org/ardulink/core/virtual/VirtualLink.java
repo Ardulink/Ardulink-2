1199059ea43a1c1da461ea13ba01f2771aad4a28
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-02-23 15:21:32 +0100

    migrated from Thread to Timer

--- ardulink-link-virtual/src/main/java/org/ardulink/core/virtual/VirtualLink.java
+++ ardulink-link-virtual/src/main/java/org/ardulink/core/virtual/VirtualLink.java
@@ -1,5 +1,21 @@
+/**
+Copyright 2013 project Ardulink http://www.ardulink.org/
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package org.ardulink.core.virtual;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static org.ardulink.core.Pin.Type.ANALOG;
 import static org.ardulink.core.Pin.Type.DIGITAL;
 import static org.ardulink.core.events.DefaultAnalogPinValueChangedEvent.analogPinValueChanged;
@@ -10,6 +26,8 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.ardulink.core.AbstractListenerLink;
@@ -21,37 +39,38 @@ import org.ardulink.core.proto.api.MessageIdHolders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * [ardulinktitle] [ardulinkversion]
+ * 
+ * project Ardulink http://www.ardulink.org/
+ * 
+ * [adsense]
+ *
+ */
 public class VirtualLink extends AbstractListenerLink {
 
 	private final Logger logger = LoggerFactory.getLogger(VirtualLink.class);
 
 	private final SecureRandom secureRandom = new SecureRandom();
 
-	private final Thread thread = new Thread() {
-		@Override
-		public void run() {
-			while (true) {
-				sendRandomMessagesAndSleep();
-			}
-		}
-	};
-
 	private final Map<Pin, Object> listeningPins = new ConcurrentHashMap<>();
 
-	private VirtualLinkConfig config;
+	private Timer timer = new Timer();
 
 	public VirtualLink(VirtualLinkConfig config) {
-		this.config = config;
-		this.thread.setDaemon(true);
-		this.thread.start();
+		this.timer.scheduleAtFixedRate(task(), 0, MILLISECONDS.convert(config.delay, config.delayUnit));
 	}
 
-	protected void sendRandomMessagesAndSleep() {
-		sendRandomPinStates();
-		config.sleep();
+	private TimerTask task() {
+		return new TimerTask() {
+			@Override
+			public void run() {
+				sendRandomPinStates();
+			}
+		};
 	}
 
-	protected void sendRandomPinStates() {
+	private void sendRandomPinStates() {
 		for (Entry<Pin, Object> entry : listeningPins.entrySet()) {
 			Pin pin = entry.getKey();
 			if (pin.is(ANALOG)) {
@@ -65,7 +84,7 @@ public class VirtualLink extends AbstractListenerLink {
 	@Override
 	public void close() throws IOException {
 		super.close();
-		this.thread.interrupt();
+		this.timer.cancel();
 	}
 
 	@Override
@@ -81,8 +100,7 @@ public class VirtualLink extends AbstractListenerLink {
 		if (pin.is(DIGITAL)) {
 			return getRandomDigital();
 		}
-		throw new IllegalStateException("Cannot handle pin type "
-				+ pin.getType() + " of pin " + pin);
+		throw new IllegalStateException("Cannot handle pin type " + pin.getType() + " of pin " + pin);
 	}
 
 	private Integer getRandomAnalog() {
@@ -100,24 +118,21 @@ public class VirtualLink extends AbstractListenerLink {
 	}
 
 	@Override
-	public long switchAnalogPin(AnalogPin analogPin, int value)
-			throws IOException {
+	public long switchAnalogPin(AnalogPin analogPin, int value) throws IOException {
 		logger.info("{} set to {}", analogPin, value);
 		return MessageIdHolders.NO_ID.getId();
 	}
 
 	@Override
-	public long switchDigitalPin(DigitalPin digitalPin, boolean value)
-			throws IOException {
+	public long switchDigitalPin(DigitalPin digitalPin, boolean value) throws IOException {
 		logger.info("{} set to {}", digitalPin, value);
 		return MessageIdHolders.NO_ID.getId();
 	}
 
 	@Override
-	public long sendKeyPressEvent(char keychar, int keycode, int keylocation,
-			int keymodifiers, int keymodifiersex) throws IOException {
-		logger.info("key pressed ({} {} {} {} {})", keychar, keycode,
-				keylocation, keymodifiers, keymodifiersex);
+	public long sendKeyPressEvent(char keychar, int keycode, int keylocation, int keymodifiers, int keymodifiersex)
+			throws IOException {
+		logger.info("key pressed ({} {} {} {} {})", keychar, keycode, keylocation, keymodifiers, keymodifiersex);
 		return MessageIdHolders.NO_ID.getId();
 	}
 
--- ardulink-link-virtual/src/main/java/org/ardulink/core/virtual/VirtualLinkConfig.java
+++ ardulink-link-virtual/src/main/java/org/ardulink/core/virtual/VirtualLinkConfig.java
@@ -1,3 +1,18 @@
+/**
+Copyright 2013 project Ardulink http://www.ardulink.org/
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package org.ardulink.core.virtual;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -8,6 +23,14 @@ import javax.validation.constraints.Positive;
 
 import org.ardulink.core.linkmanager.LinkConfig;
 
+/**
+ * [ardulinktitle] [ardulinkversion]
+ * 
+ * project Ardulink http://www.ardulink.org/
+ * 
+ * [adsense]
+ *
+ */
 public class VirtualLinkConfig implements LinkConfig {
 
 	private static final String DELAY_UNIT = "delayUnit";
@@ -18,13 +41,5 @@ public class VirtualLinkConfig implements LinkConfig {
 
 	@Named(DELAY_UNIT)
 	public TimeUnit delayUnit = MILLISECONDS;
-
-	public void sleep() {
-		try {
-			this.delayUnit.sleep(delay);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-		}
-	}
 
 }
