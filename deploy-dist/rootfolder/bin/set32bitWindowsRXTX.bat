e486c2d29c992a272edaed7ce4546baa445ee323
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-09-29 22:58:37 +0200

    purged deploy-dist/rootfolder/bin/set*bitWindowsRXTX.bat

--- deploy-dist/rootfolder/bin/set32bitWindowsRXTX.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem copy ..\winDLLs\32bit\RXTXcomm.jar ..\lib
-copy ..\winDLLs\32bit\rxtxSerial.dll ..\lib
-copy ..\winDLLs\32bit\LibusbJava.dll ..\lib
-pause
--- deploy-dist/rootfolder/bin/set64bitWindowsRXTX.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem copy ..\winDLLs\64bit\RXTXcomm.jar ..\lib
-copy ..\winDLLs\64bit\rxtxSerial.dll ..\lib
-copy ..\winDLLs\64bit\LibusbJava.dll ..\lib
-pause
